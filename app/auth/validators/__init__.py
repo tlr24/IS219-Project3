44c8d40f5b21fd3a9e553bdb8559d490cc1eac8e
tlr24 <tlr24@njit.edu>
2022-05-02 13:22:40 -0400

    Changed 'is' to == in custom validators

--- app/auth/validators/__init__.py
+++ app/auth/validators/__init__.py
@@ -4,15 +4,15 @@ from wtforms import ValidationError
 
 def validate_lowercase(form, field):
     r = re.findall("[a-z]", field.data)
-    if len(r) is 0:
+    if len(r) == 0:
         raise ValidationError('Must have lowercase letter')
 
 def validate_uppercase(form, field):
     r = re.findall("[A-Z]", field.data)
-    if len(r) is 0:
+    if len(r) == 0:
         raise ValidationError('Must have uppercase letter')
 
 def special_character(form, field):
     r = re.findall('''[`~!@#$%^&*()_|+\-=?;:'",.<>\{\}\[\]\\\/]''', field.data)
-    if len(r) is 0:
+    if len(r) == 0:
         raise ValidationError('Must have special character')
